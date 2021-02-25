c459bc4218e6c71f02bc4193d0d6e667cec2527a
DaveBraid91 <79541911+DaveBraid91@users.noreply.github.com>
2021-02-25 14:24:09 +0100

    Ejercicio12 fix

---
+++ Ejercicio12/Ejercicio12/IngenieroDeVerdad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio12
+{
+    class IngenieroDeVerdad : Ingeniero
+    {
+        public IngenieroDeVerdad()
+        {
+            Console.WriteLine("No soy un informático");
+        }
+    }
+}
